25cd99be4629443de16174c7cf9188c5f5fd5c97
Jacobnkyeh <43576836+Jacobnkyeh@users.noreply.github.com>
2019-03-05 09:21:38 -0500

    Created using Popcode: https://popcode.org

---
+++ script.js
@@ -0,0 +1,34 @@
+//Spell #1
+$(".rabbit").hide();
+
+$(".button").click(function(){
+  $(".hat").hide();
+  $(".rabbit").show();
+ 
+});
+
+
+
+//Spell #2
+$(".okcard").hide();
+  $(".button1").click(function(){
+  $(".card").fadeOut();
+  $(".okcard").show();
+  var wait;
+
+  
+});
+
+
+
+
+//Spell #3
+$(".better").hide();
+$(".button2").click(function(){
+    $(".him").slideUp();
+    $(".better").show();
+ 
+});
+
+
+
